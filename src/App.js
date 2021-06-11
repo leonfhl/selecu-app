21e8d7e3c21aa5a5d808edd8665e1b5f14034c66
Leon Hernandez <leonfhl@gmail.com>
2021-06-11 18:25:11 -0500

    button houses added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import background from './resources/fondo.jpg';
 import './App.scss';
 
+import ButtonHouse from './components/ButtonHouse.js'
 
 function App() {
   return (
     <div className="App">
       <div className="container-fluid homepage-bgimage">
         
-        <p>
-          Hello World Selecu.
-        </p>
+        <ButtonHouse src = './resources/1.png' />
+        <ButtonHouse src = './resources/4.png' />
+        <ButtonHouse src = './resources/11.png' />
+        
       </div>
     </div>
   );
---
+++ src/components/ButtonHouse.js
@@ -0,0 +1,16 @@
+import React, { Component,  } from 'react';
+
+class ButtonHouse extends Component {
+    render() {
+        return (
+
+                <div>
+                     <img src = {this.props.src}/>      
+                </div>
+                
+        );
+    }
+}
+
+
+export default ButtonHouse;
